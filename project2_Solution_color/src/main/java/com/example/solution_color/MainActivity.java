768f2f7910266148e06ba40945f73d7f9f57e0ce
Rachel McCown <rachel.mccown.14@cnu.edu>
2017-02-27 18:31:21 -0500

    The Sketchiness button works Colorize doesn't really work but code is implemented Share button attaches file succesfully

--- project2_Solution_color/src/main/java/com/example/solution_color/MainActivity.java
+++ project2_Solution_color/src/main/java/com/example/solution_color/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.solution_color;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
@@ -18,16 +20,20 @@ import android.widget.Toast;
 
 import com.library.bitmap_utilities.BitMap_Helpers;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity  {
 
     static final int REQUEST_TAKE_PHOTO = 1;
-    private Uri outputFileUri;
-    String mCurrentPhotoPath;
+    private Uri photoURI = null;
+    private Uri photo;
+    private String mCurrentPhotoPath;
     private ImageView image;
 
     @Override
@@ -59,7 +65,8 @@ public class MainActivity extends AppCompatActivity  {
                 return true;
 
             case R.id.share:
-                Toast.makeText(this, "Sharing goes here", Toast.LENGTH_SHORT).show();
+                doSend();
+                //Toast.makeText(this, "Sharing goes here", Toast.LENGTH_SHORT).show();
                 return true;
 
             case R.id.sketch:
@@ -69,15 +76,28 @@ public class MainActivity extends AppCompatActivity  {
 
             case R.id.color:
                 Bitmap bw = BitMap_Helpers.thresholdBmp(((BitmapDrawable)image.getDrawable()).getBitmap(), 5);
-                Bitmap colored = BitMap_Helpers.colorBmp(((BitmapDrawable)image.getDrawable()).getBitmap(), 10);
+                Bitmap colored = BitMap_Helpers.colorBmp(bw, 10);
                 BitMap_Helpers.merge(colored, bw);
-                image.setImageBitmap(bw);
-                //Toast.makeText(this, "Colorize goes here", Toast.LENGTH_SHORT).show();
+                image.setImageBitmap(colored);
                 return true;
             default:
                 break;
         }
         return true;
+    }
+
+    private void doSend() {
+
+        if(photo != null) {
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_STREAM, photo);
+            shareIntent.setType("image/jpeg");
+            startActivity(Intent.createChooser(shareIntent, "Share Image"));
+        } else {
+            Toast.makeText(this, "No Photo Saved", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     private void doHelp() {
@@ -109,6 +129,11 @@ public class MainActivity extends AppCompatActivity  {
 
     public void dispatchTakePictureIntent(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        /*File file = new File(Environment.getExternalStorageDirectory(), "implicit.jpg");
+        outputFileUri = Uri.fromFile(file);
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);*/
+
         // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             // Create the File where the photo should go
@@ -120,7 +145,7 @@ public class MainActivity extends AppCompatActivity  {
             }
             // Continue only if the File was successfully created
             if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
+                photoURI = FileProvider.getUriForFile(this,
                         "com.example.android.fileprovider",
                         photoFile);
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
@@ -135,12 +160,15 @@ public class MainActivity extends AppCompatActivity  {
             int targetW = image.getWidth();
             int targetH = image.getHeight();
             Bitmap bp = Camera_Helpers.loadAndScaleImage(mCurrentPhotoPath, targetW, targetH);
+            photo = getImageUri(getApplicationContext(), bp);
             image.setImageBitmap(bp);
             Camera_Helpers.saveProcessedImage(bp, mCurrentPhotoPath);
 
             //lets get rid of the image so we dont hog memory
             File file = new File(mCurrentPhotoPath);
             boolean deleted = file.delete();
+        } else {
+            Toast.makeText(this, "Error Occured", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -151,6 +179,20 @@ public class MainActivity extends AppCompatActivity  {
                 takepicture(resultCode);
                 break;
         }
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+
+    public String getRealPathFromURI(Uri uri) {
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+        return cursor.getString(idx);
     }
 
     /*public void setPic() {
