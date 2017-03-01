7b6a73503ff3456e1e0689c8b5d720880bdccec1
Rachel McCown <rachel.mccown.14@cnu.edu>
2017-02-28 19:26:18 -0500

    Final Commit

--- project2_Solution_color/src/main/java/com/example/solution_color/MainActivity.java
+++ project2_Solution_color/src/main/java/com/example/solution_color/MainActivity.java
@@ -1,13 +1,20 @@
+/**
+ * @author Rachel McCown
+ * @version 2.28.2017
+ */
+
 package com.example.solution_color;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
@@ -32,15 +39,17 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity  {
 
     static final int REQUEST_TAKE_PHOTO = 1;
+    SharedPreferences SP;
     private Uri photoURI = null;
     private Uri photo;
     private String mCurrentPhotoPath;
     private Bitmap currBMP;
     private ImageView image;
-    public String sub = "";
+    private String sub = "";
     private String message = "";
-    EditText myText2;
-    EditText myText;
+    private int sketch;
+    private int color;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,10 +59,6 @@ public class MainActivity extends AppCompatActivity  {
         setSupportActionBar(toolbar);
         image = (ImageView) findViewById(R.id.camera_pic);
         image.setScaleType(ImageView.ScaleType.FIT_XY);
-
-        //setSubjectText();
-        //setShareText();
-
     }
 
     @Override
@@ -79,13 +84,17 @@ public class MainActivity extends AppCompatActivity  {
                 return true;
 
             case R.id.sketch:
-                currBMP = BitMap_Helpers.thresholdBmp(((BitmapDrawable)image.getDrawable()).getBitmap(), 10);
+                SP = PreferenceManager.getDefaultSharedPreferences(this);
+                sketch = Integer.parseInt(SP.getString("sketchiness", "0"));
+                currBMP = BitMap_Helpers.thresholdBmp(((BitmapDrawable)image.getDrawable()).getBitmap(), sketch);
                 image.setImageBitmap(currBMP);
                 photo = getImageUri(getApplicationContext(), currBMP);
                 return true;
 
             case R.id.color:
-                Bitmap bw = BitMap_Helpers.thresholdBmp(((BitmapDrawable)image.getDrawable()).getBitmap(), 45);
+                SP = PreferenceManager.getDefaultSharedPreferences(this);
+                color = Integer.parseInt(SP.getString("saturation", "0"));
+                Bitmap bw = BitMap_Helpers.thresholdBmp(((BitmapDrawable)image.getDrawable()).getBitmap(), color);
                 Bitmap colored = BitMap_Helpers.colorBmp(currBMP, 120);
                 BitMap_Helpers.merge(bw, colored);
                 currBMP = colored;
@@ -98,30 +107,11 @@ public class MainActivity extends AppCompatActivity  {
         return true;
     }
 
-    /*
-    private void getShareText() {
-        myText = (EditText)findViewById(R.id.share_text);
-        message = myText.getText().toString();
-    }
-
-    private void setShareText() {
-        myText = (EditText)findViewById(R.id.share_text);
-        myText.setText("");
-    }
-
-    private void getSubjectText() {
-        myText2 = (EditText)findViewById(R.id.subject_text);
-        sub = myText2.getText().toString();
-    }
-
-    private void setSubjectText() {
-        myText2 = (EditText)findViewById(R.id.subject_text);
-        myText2.setText("");
-    }
-
-    */
-
     private void doSend() {
+
+        SP = PreferenceManager.getDefaultSharedPreferences(this);
+        sub = SP.getString("subject_text", "N/A");
+        message = SP.getString("share_text", "N/A");
 
         if(photo != null) {
             Intent shareIntent = new Intent();
@@ -231,29 +221,5 @@ public class MainActivity extends AppCompatActivity  {
         int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
         return cursor.getString(idx);
     }
-
-    /*public void setPic() {
-        // Get the dimensions of the View
-        int targetW = image.getWidth();
-        int targetH = image.getHeight();
-
-        // Get the dimensions of the bitmap
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-        int photoW = bmOptions.outWidth;
-        int photoH = bmOptions.outHeight;
-
-        // Determine how much to scale down the image
-        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
-
-        // Decode the image file into a Bitmap sized to fill the View
-        bmOptions.inJustDecodeBounds = false;
-        bmOptions.inSampleSize = scaleFactor;
-        bmOptions.inPurgeable = true;
-
-        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-        image.setImageBitmap(bitmap);
-    }*/
 }
 
